0040b0a15f90fbe23905cd2c53761290632d32c0
Alberto Basalo <albertobasalo@hotmail.com>
2021-08-24 12:20:06 +0200

    chore: fix geenrated specs

--- src/app/shared/util/store.spec.ts
+++ src/app/shared/util/store.spec.ts
@@ -2,6 +2,6 @@ import { Store } from './store';
 
 describe('Store', () => {
   it('should create an instance', () => {
-    expect(new Store()).toBeTruthy();
+    expect(new Store<number>(0)).toBeTruthy();
   });
 });
